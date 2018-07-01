d84ec2ddeb564fbe9dcec6c3bef733860d3fd77c
Kamil <kamilwitekpir2@gmail.com>
2018-07-01 17:07:17 +0200

    Add files via upload
    Loks like layout from wyzwanie90dni.pl

---
+++ script.js
@@ -0,0 +1,18 @@
+var set = 0;
+window.onresize = function(){
+  document.getElementById("menu1").style.visibility = "hidden";
+  document.getElementById("menu1").style.padding = "0px";
+  set = 0;
+}
+document.getElementById("stripes").onclick = function(){
+  if(set == 1){
+    document.getElementById("menu1").style.visibility = "hidden";
+    set = 0;
+    document.getElementById("menu1").style.padding = "0px";
+  }
+  else{
+    document.getElementById("menu1").style.visibility = "visible";
+    set = 1;
+    document.getElementById("menu1").style.padding = "120px";
+  }
+};
